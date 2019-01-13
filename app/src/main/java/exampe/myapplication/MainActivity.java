c3e2222bd5d1bd31ed6ec479ee37e2df31c9a090
mozuneng <1468033693@qq.com>
2019-01-13 20:24:36 +0800

    new

--- app/src/main/java/exampe/myapplication/MainActivity.java
+++ app/src/main/java/exampe/myapplication/MainActivity.java
@@ -4,10 +4,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,24 +18,24 @@ public class MainActivity extends AppCompatActivity {
     //view指的是被点击的按钮Button
     public void onLogin(View view){
 
-        //获取用户名和密码的控件
-        EditText username = (EditText) findViewById(R.id.id_name);
-        EditText password = (EditText) findViewById(R.id.id_password);
-
-        //接收两个控件的内容(getText())，并转换成字符串(toString())，把空格去掉(trim())
-        String usernString = username.getText().toString().trim();
-        String passwordString = password.getText().toString().trim();
-
-        //打印接收到的数据
-        Log.e("测试","username="+usernString+",password="+passwordString);
-
-        //模拟用户登录
-        if(usernString.equals("mmm") && passwordString.equals("123")){
-            Toast.makeText(this,"登录成功",Toast.LENGTH_SHORT).show();
-        }else {
-            Toast.makeText(this,"用户名/密码错误",Toast.LENGTH_SHORT).show();
-        }
-
+//        //获取用户名和密码的控件
+//        EditText username = (EditText) findViewById(R.id.id_name);
+//        EditText password = (EditText) findViewById(R.id.id_password);
+//
+//        //接收两个控件的内容(getText())，并转换成字符串(toString())，把空格去掉(trim())
+//        String usernString = username.getText().toString().trim();
+//        String passwordString = password.getText().toString().trim();
+//
+//        //打印接收到的数据
+//        Log.e("测试","username="+usernString+",password="+passwordString);
+//
+//        //模拟用户登录
+//        if(usernString.equals("mmm") && passwordString.equals("123")){
+//            Toast.makeText(this,"登录成功",Toast.LENGTH_SHORT).show();
+//        }else {
+//            Toast.makeText(this,"用户名/密码错误",Toast.LENGTH_SHORT).show();
+//        }
+//
 
 
 //        Toast.makeText(this,"登录处理..",Toast.LENGTH_SHORT).show();
