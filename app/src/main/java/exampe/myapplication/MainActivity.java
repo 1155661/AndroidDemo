c0e16daf13d5ede620acda8d5543b7dca840ffd3
mozuneng <1468033693@qq.com>
2019-01-13 20:51:29 +0800

    new

--- app/src/main/java/exampe/myapplication/MainActivity.java
+++ app/src/main/java/exampe/myapplication/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,31 +16,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //取得按钮对象
+        Button button = (Button) findViewById(R.id.id_buy);
+
+        //创建一个监听器
+        MyListener listener = new MyListener();
+
+        //给按钮设置一个监听器，当按钮被点击时，系统将调用listener.onClick()方法处理
+        button.setOnClickListener(listener);
     }
 
-    //view指的是被点击的按钮Button
-    public void onLogin(View view){
+    //在View类里定义了一个内部接口：OnClickListener
+    private class MyListener implements View.OnClickListener{
 
-//        //获取用户名和密码的控件
-//        EditText username = (EditText) findViewById(R.id.id_name);
-//        EditText password = (EditText) findViewById(R.id.id_password);
-//
-//        //接收两个控件的内容(getText())，并转换成字符串(toString())，把空格去掉(trim())
-//        String usernString = username.getText().toString().trim();
-//        String passwordString = password.getText().toString().trim();
-//
-//        //打印接收到的数据
-//        Log.e("测试","username="+usernString+",password="+passwordString);
-//
-//        //模拟用户登录
-//        if(usernString.equals("mmm") && passwordString.equals("123")){
-//            Toast.makeText(this,"登录成功",Toast.LENGTH_SHORT).show();
-//        }else {
-//            Toast.makeText(this,"用户名/密码错误",Toast.LENGTH_SHORT).show();
-//        }
-//
+        @Override
+        public void onClick(View v) {
+            EditText edit = (EditText) MainActivity.this.findViewById(R.id.id_count);
+            String amount = edit.getText().toString();
+            String message = "购买" + amount + "件商品";
 
+            Toast.makeText(MainActivity.this,message,Toast.LENGTH_SHORT).show();
 
-//        Toast.makeText(this,"登录处理..",Toast.LENGTH_SHORT).show();
+        }
     }
 }
